7e51c650379a7e1abda4f879e9263930daf9c030
Altayeb <m.altayeb.aldaher@gmail.com>
2026-04-04 20:34:50 +0300

    Added proper mvc structure

---
+++ app/Models/Job.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+
+
+class Job 
+{
+    public static function all(){
+        
+    return [
+        ['title' => 'Software Engineer',  'salary' => '$1000'],
+        ['title' => 'Graphic Designer', 'salary' => '$2000']
+        ];
+    }
+}
